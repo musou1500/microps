d80b3c7c156fef9174ef4947fcbe823689e4c7f5
musou1500 <musou1500@gmail.com>
2026-01-07 23:00:57 +0900

    step2 complete

--- net.c
+++ net.c
@@ -50,6 +50,15 @@ net_device_open(struct net_device *dev)
         infof("device %s is already UP", dev->name);
         return -1;
     }
+
+    if (dev->ops->open)
+    {
+        if (dev->ops->open(dev) == -1)
+        {
+            errorf("dev->ops->open() failure");
+            return -1;
+        }
+    }
     dev->flags |= NET_DEVICE_FLAG_UP;
     return 0;
 }
@@ -62,6 +71,15 @@ net_device_close(struct net_device *dev)
     {
         infof("device %s is already DOWN", dev->name);
         return -1;
+    }
+
+    if (dev->ops->close)
+    {
+        if (dev->ops->close(dev) == -1)
+        {
+            errorf("dev->ops->close() failure");
+            return -1;
+        }
     }
     dev->flags &= ~NET_DEVICE_FLAG_UP;
     return 0;
@@ -82,11 +100,27 @@ int net_device_output(struct net_device *dev, uint16_t type, const uint8_t *data
         errorf("packet size %zu exceeds MTU %u", len, dev->mtu);
         return -1;
     }
+
+    if (!dev->ops->output)
+    {
+        errorf("dev->ops->output() is NULL");
+        return -1;
+    }
+
+    if (dev->ops->output(dev, type, data, len, dst) == -1)
+    {
+        errorf("dev->ops->output() failure");
+        return -1;
+    }
+
     return 0;
 }
 
 int net_input(uint16_t type, const uint8_t *data, size_t len, struct net_device *dev)
 {
+    debugf("dev=%s, type=0x%04x, len=%zu", dev->name, type, len);
+    debugdump(data, len);
+    return 0;
 }
 
 int net_init(void)
